cd374475f06a61e169487613b2012ec518412f83
Pouya Hamraz <110659145+hadish100@users.noreply.github.com>
2024-02-02 15:09:38 +0330

    Add files via upload

--- run.sh
+++ run.sh
@@ -23,3 +23,8 @@ npm install
 node config.js
 cd ../
 docker compose up -d
+
+echo 'alias mnode_cli_restart="cd ~/Marzban-node && docker compose down && docker compose up"' >> ~/.bashrc
+
+
+# curl -fsSL https://raw.githubusercontent.com/KN-VPN/node-cli/main/run.sh -o run.sh && chmod +x run.sh && ./run.sh
